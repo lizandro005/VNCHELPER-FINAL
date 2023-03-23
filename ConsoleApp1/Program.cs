1c4b896157e693fc2e28b5d560e787fae9a9936b
Lizandro <95085892+lizandro005@users.noreply.github.com>
2023-03-23 17:41:25 -0100

    first dsi development, check ram needs to be updated

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 using System.Management;
 using System.Collections.Generic;
@@ -18,6 +18,7 @@ using System.Security.Policy;
 using System.Runtime.Remoting.Lifetime;
 
 
+
 namespace ConsoleApp1
 {
     internal class Program
@@ -31,36 +32,71 @@ namespace ConsoleApp1
         private const int SW_HIDE = 0;
         static void Main(string[] args)
         {
-            var handle = GetConsoleWindow();
-            ShowWindow(handle, SW_HIDE);
+          //  var handle = GetConsoleWindow();
+          //  ShowWindow(handle, SW_HIDE);
 
             OutputDiskCUsage();
+      
             UptimeChecker uptimeChecker = new UptimeChecker();
-            uptimeChecker.CheckUptime();
+            uptimeChecker.CheckUptime(); 
             CheckRamUsage();
+
             
-
-
-
             void CheckRamUsage()
-            {
+{
                 FileWriting fileWriting = new FileWriting();
                 try
                 {
                     var performanceCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
                     var ramUsagePercentage = performanceCounter.NextValue();
-                    string ramusage = "RAM Usage: " + ramUsagePercentage + "%";
-                    fileWriting.WriteToFile(ramusage);
+                    string ramUsageString = "RAM Usage: " + ramUsagePercentage + "%";
+                    fileWriting.WriteToFile(ramUsageString);
+
+                    // Check if RAM usage is high (e.g., over 90%)
+                    if (ramUsagePercentage > 0)
+                    {
+                        // Prompt user to clean up RAM
+                        Console.WriteLine("Your computer's RAM usage is high. Cleaning up RAM may help improve performance, but it may also close some programs.");
+                        Console.Write("Do you want to clean up RAM now? (Y/N): ");
+                        string answer = Console.ReadLine().ToLower();
+
+                        if (answer == "s" || answer == "sim")
+                        {
+                            // Terminate Microsoft Edge instances
+                            foreach (Process p in Process.GetProcessesByName("Microsoft.Photos.exe"))
+                            {
+                                p.Kill();
+                            }
+                        }
+                    }
                 }
                 catch (Exception ex)
                 {
-                    fileWriting.WriteToFile("Error: Unable to retrieve RAM usage data." + ex.Message);
+                    fileWriting.WriteToFile("Error: Unable to retrieve RAM usage data. " + ex.Message);
                 }
             }
 
 
-        
 
+
+
+            /*
+                        void CheckRamUsage()
+                        {
+                            FileWriting fileWriting = new FileWriting();
+                            try
+                            {
+                                var performanceCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+                                var ramUsagePercentage = performanceCounter.NextValue();
+                                string ramusage = "RAM Usage: " + ramUsagePercentage + "%";
+                                fileWriting.WriteToFile(ramusage);
+                            }
+                            catch (Exception ex)
+                            {
+                                fileWriting.WriteToFile("Error: Unable to retrieve RAM usage data." + ex.Message);
+                            }
+                        }
+            */
 
 
             void OutputDiskCUsage()
@@ -73,7 +109,53 @@ namespace ConsoleApp1
                 string freediskspace = "\n Disk C: Usage: " + usedSpace + " GB out of " + totalSize + " GB";
                 fileWriting.WriteToFile(freediskspace);
 
+                double freeSpacePercentage = (freeSpace / totalSize) * 100;
+                string teste = freeSpacePercentage.ToString();
+                fileWriting.WriteToFile(teste);
+                if (freeSpacePercentage < 95)
+                {
+                    //string tempFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Temp";
+                    string tempFolderPath = ("C:\\Users\\goncalo.o.lizandro\\Desktop\\aiai"); // para testes <-
+                    if (Directory.Exists(tempFolderPath))
+                    {
+                        DirectoryInfo tempDir = new DirectoryInfo(tempFolderPath);
+                        foreach (FileInfo file in tempDir.GetFiles())
+                        {
+                            try
+                            {
+                                file.Delete();
+                            }
+                            catch (Exception ex)
+                            {
+                                fileWriting.WriteToFile("Nada" + ex.Message);
+                            }
+                        }
+                        foreach (DirectoryInfo subDir in tempDir.GetDirectories())
+                        {
+                            try
+                            {
+                                subDir.Delete(true);
+                            }
+                            catch (Exception ex)
+                            {
+                                fileWriting.WriteToFile("Pastas" + ex.Message);
+                            }
+                        }
+                    }
+                }
             }
+        }
+
+
+
+
+
+
+        
+
+
+
+           
            
         }
 
@@ -86,6 +168,7 @@ namespace ConsoleApp1
 
 
 
+
             public void CheckUptime()
             {
                 try
@@ -95,6 +178,21 @@ namespace ConsoleApp1
                     string uptimeString = $"Uptime: {uptime.Days} days {uptime.Hours} hours {uptime.Minutes} minutes {uptime.Seconds} seconds";
                     FileWriting fileWriting = new FileWriting();
                     fileWriting.WriteToFile(uptimeString);
+            
+                    // Check if uptime is more than 3 days
+                    if (uptime.TotalDays > 0)
+                    {
+                        // Prompt user to restart the machine
+                        Console.WriteLine("O seu computador está ligado à mais de 3 dias. É recomendado que reinicie o seu computador para assegurar máxima perfomance.");
+                        Console.Write("Quer reiniciar o seu computador agora? (s/n): ");
+                        string answer = Console.ReadLine().ToLower();
+            
+                        if (answer == "s" || answer == "sim")
+                        {
+                            // Restart the machine
+                            Process.Start("shutdown", "/r /t 0");
+                        }
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -102,10 +200,9 @@ namespace ConsoleApp1
                     fileWriting.WriteToFile("Error: Unable to retrieve uptime data. " + ex.Message);
                 }
             }
-        }
+    }
 
   
-
 
 
 
@@ -134,4 +231,4 @@ namespace ConsoleApp1
     }
   
 
-}
+
